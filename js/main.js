5cb0634463dfbe476a5876510db1ef113820b015
unknown <balamine129@gmail.com>
2023-06-07 10:38:32 +0000

    balise p du deuxieme section

--- js/main.js
+++ js/main.js
@@ -179,3 +179,4 @@ function toggle_div( btn, id_div )
   btn.title = (btn.title == 'Afficher la suite')? 'Masquer la suite' : 'Afficher la suite';
   document.getElementById( id_div ).style.display = (document.getElementById( id_div ).style.display == 'none')? 'block' : 'none';
 }
+
